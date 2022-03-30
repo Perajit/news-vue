5229c44009b19aa0e41bee7f9edbe0c9ef637825
Perajit Pasupat <perajit.p@gmail.com>
2022-03-30 13:54:54 +0700

    chore: add vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import * as VuetifyComponents from 'vuetify/lib/components';
+
+Vue.use(Vuetify, {
+  components: VuetifyComponents,
+});
+
+export default new Vuetify({
+});
